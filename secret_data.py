940b961ad396442d42c9386ecb0050637489690d
ehmatthes <sophia.world1992@gmail.com>
2017-12-12 00:19:17 -0500

    update secret_data

---
+++ secret_data.py
@@ -0,0 +1,10 @@
+
+# Fill these in:
+# Get these from the Yelp website, by going to
+# https://www.yelp.com/developers/v3/manage_app and create an "app"
+# Visit the Keys and Access Tokens tab for your app and grab the following two values
+
+
+client_id = "input your API Key here"
+client_secret = "input your Client Secret here "
+
